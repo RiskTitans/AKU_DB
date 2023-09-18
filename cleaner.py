f12217fffadb280eaf77ade7c9401f49b596ea47
Arslan Vezirov <arslan.vezirov@gmail.com>
2023-09-18 19:00:45 +0300

    dsn_wd code created uploaded file list created files are uploaded to DB

---
+++ cleaner.py
@@ -0,0 +1,206 @@
+import os
+import pandas as pd
+import time
+# folder_path = r'C:\Users\yuriy\Desktop\db_tables\13_work_documentation\test'
+dd_list_cols = {
+    'id_obj': 'id_obj',
+    'Текущая ревизия': 'current_rev',
+    'Статус текущей ревизии': 'status_current_rev',
+    'Дата статуса': 'status_date',
+    'Письма': 'letter',
+    'Статус Заказчика': 'status_company',
+    'Дата статуса Заказчика': 'status_date_company',
+    'Первый статус по выпуску РД': 'first_status_wd',
+    'Дата выпуска РД': 'release_date_wd',
+    'Ревизия, выданная в производство': 'rev_onproduction',
+    'Дата выдачи в производство': 'date_toproduction',
+    'Письмо о выдаче в производство': 'letter_toproduction',
+    'Код KKS базисной сметы': 'basesmeta_kks',
+    'Код KKS ресурсной сметы': 'resourcesmeta_kks',
+    'Ожидаемая дата получения РД от разработчика': 'expected_developer_date',
+    'Источник информации': 'info_source',
+    'Ожидаемая дата выдачи РД в производство': 'expected_toproduction_date',
+    'Дата выпуска РД по договору подрядчика': 'contract_toproduction_date',
+    'Дата выпуска РД по графику с Заказчиком': 'company_toproduction_date',
+    'Статус РД в 1С': 'status_1c',
+    'expected_developer_status': 'expected_developer_status',
+    'expected_toproduction_status': 'expected_toproduction_status',
+    # 'source_file': 'source_file'
+}
+wd_cols = {
+    'id_obj': 'id_obj',
+    'Наименование объекта/комплекта РД': 'wd_name',
+    'Коды работ по выпуску РД': 'wd_code',
+    'Пакет РД': 'wd_batch',
+    'Код KKS документа': 'wd_kks',
+    'designer_code': 'designer_code',
+    'Объект': 'object',
+    'object_name': 'object_name',
+    'Группа': 'unit',
+    'WBS': 'wbs',
+    'wbs_name': 'wbs_name',
+    'Код работы СМР': 'smr_code',
+    'Вид работ': 'dd_type',
+}
+smr_cols = {
+    'id_obj': 'id_obj',
+    'Код работы СМР': 'smr_code',
+    'Наименование работы СМР': 'smr_name',
+    'Дата старта СМР': 'smr_start_date',
+}
+designers_cols = {
+    'designer_code': 'designer_code',
+    'Разработчики РД (только основной договор)': 'developer_contract',
+    'Разработчики РД (актуальные)': 'developer_actual',
+}
+base_smeta_cols = {
+    'id_obj': 'id_obj',
+    'Код KKS базисной сметы': 'basesmeta_kks',
+    'Номер базисной сметы': 'basesmeta_no',
+    'Ревизия базисной сметы': 'basesmeta_rev',
+    'Статус базисной сметы': 'basesmeta_status',
+    'Дата статуса базисной сметы': 'basesmeta_date',
+}
+resource_smeta_cols = {
+    'id_obj': 'id_obj',
+    'Код KKS ресурсной сметы': 'resourcesmeta_kks',
+    'Номер ресурсной сметы': 'resourcesmeta_no',
+    'Ревизия ресурсной сметы': 'resourcesmeta_rev',
+    'Статус ресурсной сметы': 'resourcesmeta_status',
+    'Дата статуса ресурсной сметы': 'resourcesmeta_date',
+}
+all_cols = {**dd_list_cols,
+            **wd_cols,
+            **smr_cols,
+            **designers_cols,
+            **base_smeta_cols,
+            **resource_smeta_cols}
+
+
+def clean_excel(file_path, all_cols):
+    save_path = r'C:\Users\yuriy\Desktop\db_tables\13_work_documentation\upload_files'
+    filename = os.path.basename(file_path)
+    save_file_path = os.path.join(save_path, filename)
+
+    def compare_excel_columns(file_cols, base_cols):
+        # file_cols and base_cols are set format
+
+        # finds differences in columns
+        only_in_base = base_cols.difference(file_cols)
+        only_in_file = file_cols.difference(base_cols)
+
+        return only_in_base, only_in_file
+
+    try:
+        start = time.time()
+        # read xlsx Excel file
+        excel_df = pd.read_excel(file_path, sheet_name=None)
+
+        # check sheet names if Итого exist drop other sheets else merge Blok 1-2-3-4
+        sheet_names = list(excel_df.keys())
+        if 'Итого' in sheet_names:
+            df = (excel_df['Итого'])
+        elif all(block in sheet_names for block in ['блок 1', 'блок 2', 'блок 3', 'блок 4']):
+            df = (pd.concat([excel_df[block] for block in ['блок 1', 'блок 2', 'блок 3', 'блок 4']],
+                            axis=0, ignore_index=True, sort=False))
+        else:
+            return None
+
+        # check column names compare with basefile columns (basefile columns added manually)
+        base_columns = {
+            'Наименование объекта/комплекта РД', 'Коды работ по выпуску РД',
+            'Пакет РД', 'Код KKS документа', 'Текущая ревизия',
+            'Статус текущей ревизии', 'Дата статуса', 'Письма', 'Статус Заказчика',
+            'Дата статуса Заказчика', 'Первый статус по выпуску РД',
+            'Дата выпуска РД', 'Ревизия, выданная в производство',
+            'Дата выдачи в производство', 'Письмо о выдаче в производство',
+            'Код KKS базисной сметы', 'Номер базисной сметы',
+            'Ревизия базисной сметы', 'Статус базисной сметы',
+            'Дата статуса базисной сметы', 'Код KKS ресурсной сметы',
+            'Номер ресурсной сметы', 'Ревизия ресурсной сметы',
+            'Статус ресурсной сметы', 'Дата статуса ресурсной сметы',
+            'Ожидаемая дата получения РД от разработчика', 'Источник информации',
+            'Ожидаемая дата выдачи РД в производство',
+            'Разработчики РД (только основной договор)',
+            'Разработчики РД (актуальные)',
+            'Дата выпуска РД по договору подрядчика',
+            'Дата выпуска РД по графику с Заказчиком', 'Дата старта СМР',
+            'Код работы СМР', 'Наименование работы СМР',
+            'id_obj', 'Объект', 'Группа', 'WBS',
+            'Вид работ', 'Статус РД в 1С', 'Нужна в графике СМР'}
+
+        file_columns = set(df.columns)
+
+        only_base, only_file = compare_excel_columns(file_columns, base_columns)
+        if only_file and only_base:
+            print(f'\nDifferences found in file: {filename}\n')
+            print("Columns only in base_file :\n")
+            print("\n".join(only_base) + '\n')
+            print('-' * 30)
+            print("\nColumns only in file :\n")
+            print("\n".join(only_file) + '\n')
+            print('=' * 30 + '\n')
+            return None
+
+        # rename columns, align columns and drop unused and duplicated columns
+        df = (df.rename(columns=all_cols))
+
+        # Drop empty cells in 'wd_code' and align column types according to database
+        df['id_obj'] = pd.to_numeric(df['id_obj'], errors='coerce')
+        df.dropna(subset=['wd_code', 'id_obj'], inplace=True)
+        df['id_obj'] = df['id_obj'].astype('int64')
+        df = (df.drop_duplicates(subset="id_obj", keep='last', ignore_index=True)
+              .set_index(["id_obj"])
+              .sort_index()
+              .reset_index())
+
+        # df = df.drop_duplicates(subset='id_obj',  keep='last', ignore_index=True).reset_index(drop=True)
+
+        date_cols = ['status_date',
+                     'status_date_company',
+                     'release_date_wd',
+                     'date_toproduction',
+                     'contract_toproduction_date',
+                     'company_toproduction_date',
+                     'smr_start_date',
+                     'basesmeta_date',
+                     'resourcesmeta_date'
+                     ]
+        for col in date_cols:
+            df[col] = pd.to_datetime(df[col], errors='coerce')
+
+        # Separate columns ['expected_developer_date', 'expected_toproduction_date']
+        #seperate mixed data datetime and str
+        df['expected_developer_status'] = df['expected_developer_date'].where(
+            pd.to_datetime(df['expected_developer_date'],
+                           errors='coerce').isna())
+        df['expected_developer_date'] = pd.to_datetime(df['expected_developer_date'],
+                                                       errors='coerce')
+
+        df['expected_toproduction_status'] = df['expected_toproduction_date'].where(
+            pd.to_datetime(df['expected_toproduction_date'],
+                           errors='coerce').isna())
+        df['expected_toproduction_date'] = pd.to_datetime(df['expected_toproduction_date'],
+                                                          errors='coerce')
+
+        #organize code
+        df['designer_code'] = df['wd_kks'].str.extract(r'AKU\.(\d{4})')
+        df[['object', 'object_name']] = df['object'].str.split(' - ', 1, expand=True)
+        df[['wbs', 'wbs_name']] = df['wbs'].str.split(' - ', 1, expand=True)
+
+        end = time.time()
+        print('Total time: ', end-start)
+
+        df.to_excel(save_file_path, sheet_name='Итого', columns=all_cols.values(), index=False)
+        return df
+    except Exception as e:
+        print(f"Error processing {filename}: {str(e)}")
+        return None
+
+
+#debug
+# if __name__ == "__main__":
+#     folder_path = r'C:\Users\yuriy\Desktop\db_tables\13_work_documentation\xlsx_files'
+#     file_name = 'Отчет 20.1 по статусам РД (бл.1-4) на 2023.09.11.xlsx'
+#     file_path = os.path.join(folder_path, file_name)
+#     clean_excel(file_path, all_cols)
---
+++ dsn_db/dsn_wd2.py
@@ -0,0 +1,243 @@
+import pandas as pd
+import os
+import time
+from sqlalchemy import create_engine
+from cleaner import clean_excel
+
+# Connect to the PostgreSQL database
+db_url = 'postgresql://postgres:postgres@localhost:5432/DSN_DB'
+engine_dsn = create_engine(db_url)
+
+
+folder_path = r'C:\Users\yuriy\Desktop\db_tables\13_work_documentation\upload_files'
+dd_list_cols = {
+    'id_obj': 'id_obj',
+    'Текущая ревизия': 'current_rev',
+    'Статус текущей ревизии': 'status_current_rev',
+    'Дата статуса': 'status_date',
+    'Письма': 'letter',
+    'Статус Заказчика': 'status_company',
+    'Дата статуса Заказчика': 'status_date_company',
+    'Первый статус по выпуску РД': 'first_status_wd',
+    'Дата выпуска РД': 'release_date_wd',
+    'Ревизия, выданная в производство': 'rev_onproduction',
+    'Дата выдачи в производство': 'date_toproduction',
+    'Письмо о выдаче в производство': 'letter_toproduction',
+    'Код KKS базисной сметы': 'basesmeta_kks',
+    'Код KKS ресурсной сметы': 'resourcesmeta_kks',
+    'Ожидаемая дата получения РД от разработчика': 'expected_developer_date',
+    'Источник информации': 'info_source',
+    'Ожидаемая дата выдачи РД в производство': 'expected_toproduction_date',
+    'Дата выпуска РД по договору подрядчика': 'contract_toproduction_date',
+    'Дата выпуска РД по графику с Заказчиком': 'company_toproduction_date',
+    'Статус РД в 1С': 'status_1c',
+    'expected_developer_status': 'expected_developer_status',
+    'expected_toproduction_status': 'expected_toproduction_status',
+    # 'source_file': 'source_file'
+}
+wd_cols = {
+    'id_obj': 'id_obj',
+    'Наименование объекта/комплекта РД': 'wd_name',
+    'Коды работ по выпуску РД': 'wd_code',
+    'Пакет РД': 'wd_batch',
+    'Код KKS документа': 'wd_kks',
+    'designer_code': 'designer_code',
+    'Объект': 'object',
+    'object_name': 'object_name',
+    'Группа': 'unit',
+    'WBS': 'wbs',
+    'wbs_name': 'wbs_name',
+    'Код работы СМР': 'smr_code',
+    'Вид работ': 'dd_type',
+}
+smr_cols = {
+    'id_obj': 'id_obj',
+    'Код работы СМР': 'smr_code',
+    'Наименование работы СМР': 'smr_name',
+    'Дата старта СМР': 'smr_start_date',
+}
+designers_cols = {
+    'designer_code': 'designer_code',
+    'Разработчики РД (только основной договор)': 'developer_contract',
+    'Разработчики РД (актуальные)': 'developer_actual',
+}
+base_smeta_cols = {
+    'id_obj': 'id_obj',
+    'Код KKS базисной сметы': 'basesmeta_kks',
+    'Номер базисной сметы': 'basesmeta_no',
+    'Ревизия базисной сметы': 'basesmeta_rev',
+    'Статус базисной сметы': 'basesmeta_status',
+    'Дата статуса базисной сметы': 'basesmeta_date',
+}
+resource_smeta_cols = {
+    'id_obj': 'id_obj',
+    'Код KKS ресурсной сметы': 'resourcesmeta_kks',
+    'Номер ресурсной сметы': 'resourcesmeta_no',
+    'Ревизия ресурсной сметы': 'resourcesmeta_rev',
+    'Статус ресурсной сметы': 'resourcesmeta_status',
+    'Дата статуса ресурсной сметы': 'resourcesmeta_date',
+}
+all_cols = {**dd_list_cols,
+            **wd_cols,
+            **smr_cols,
+            **designers_cols,
+            **base_smeta_cols,
+            **resource_smeta_cols}
+
+
+def compare_dfs(df, db_df, table_name, pk):
+    # df is excel data, db_df is database data
+    # find new rows and changed rows in df_dd_list
+    df = df.where(pd.notna(df), None)
+    start = time.time()
+    aligned_df = db_df[[col for col in df.columns]]
+    df['source'] = 'new'
+    aligned_df['source'] = 'old'
+    # source 'new' and 'old' need for getting rows only from excel file
+    # (some excel files have missing rows compared to database
+    concatenated_df = pd.concat([df, aligned_df], ignore_index=True)
+
+    if table_name == 'designers':
+        new_rows = concatenated_df.drop_duplicates(subset=pk, keep='last', ignore_index=True)
+        new_rows = new_rows[new_rows['source'] != 'old']
+        new_rows = new_rows.drop('source', axis=1)
+    else:
+        new_rows = concatenated_df.drop_duplicates(subset=pk, keep=False, ignore_index=True)
+        new_rows = new_rows[new_rows['source'] != 'old']
+        new_rows = new_rows.drop('source', axis=1)
+
+    # na filled with 'test' because in comparison (none == none) -> False
+    concatenated_df = concatenated_df.drop(['source'], axis=1).fillna('test')
+    duplicate_rows = (concatenated_df[concatenated_df.duplicated(keep=False)])
+    changed_rows = df[~df[pk].isin(new_rows[pk]) & ~df[pk].isin(duplicate_rows[pk])]
+    changed_rows = changed_rows.drop(['source'], axis=1)
+    end = time.time()
+    print('Comparison time:', end - start)
+    return new_rows, changed_rows, db_df
+
+
+def excel_dfs(file_path):
+
+    df_excel = clean_excel(file_path, all_cols)
+    # split df into df for each table from db
+    start = time.time()
+    df_dd_list = df_excel[[col for col in dd_list_cols.values() if col in df_excel.columns]]
+    df_designers = (df_excel[[col for col in designers_cols.values() if col in df_excel.columns]]
+                    .dropna(axis=0, how='any')
+                    .drop_duplicates(subset="designer_code", keep='last', ignore_index=True)
+                    .set_index(["designer_code"])
+                    .sort_index()
+                    .reset_index())
+
+    df_wd = df_excel[[col for col in wd_cols.values() if col in df_excel.columns]]
+    df_smr = df_excel[[col for col in smr_cols.values() if col in df_excel.columns]]
+    df_base_smeta = df_excel[[col for col in base_smeta_cols.values() if col in df_excel.columns]]
+    df_resource_smeta = df_excel[[col for col in resource_smeta_cols.values() if col in df_excel.columns]]
+    df_excel_list = {
+        'dd_list': df_dd_list,
+        'wd': df_wd,
+        'smr': df_smr,
+        'designers': df_designers,
+        'smeta_base': df_base_smeta,
+        'smeta_resource': df_resource_smeta
+    }
+    end = time.time()
+    print('Excel dataframes init time:', end - start)
+    return df_excel_list
+
+
+def sql_dfs():
+    start = time.time()
+    # sql queries
+    sql_dd_list = 'SELECT * FROM dd_list'
+    sql_wd = 'SELECT * FROM wd'
+    sql_smr = 'SELECT * FROM smr'
+    sql_designers = 'SELECT * FROM designers'
+    sql_base_smeta = 'SELECT * FROM smeta_base'
+    sql_resource_smeta = 'SELECT * FROM smeta_resource'
+
+    df_dd_list_db = (pd.read_sql_query(sql_dd_list, engine_dsn)
+                     .drop(columns='source_file')
+                     .drop_duplicates(keep='last')
+                     .set_index(['id_obj'])
+                     .sort_index()
+                     .reset_index()
+                     )
+    df_wd_db = pd.read_sql_query(sql_wd, engine_dsn)
+    df_smr_db = pd.read_sql_query(sql_smr, engine_dsn)
+    df_designers_db = pd.read_sql_query(sql_designers, engine_dsn)
+    df_smeta_base_db = pd.read_sql_query(sql_base_smeta, engine_dsn)
+    df_smeta_resource_db = pd.read_sql_query(sql_resource_smeta, engine_dsn)
+
+    df_sql_list = {
+        'dd_list': df_dd_list_db,
+        'wd': df_wd_db,
+        'smr': df_smr_db,
+        'designers': df_designers_db,
+        'smeta_base': df_smeta_base_db,
+        'smeta_resource': df_smeta_resource_db
+    }
+    end = time.time()
+    print('sql dataframes init time:', end - start)
+    return df_sql_list
+
+
+def upload_to_sql(file_name, file_path):
+
+    excel_df = excel_dfs(file_path)  # dict table name: table df
+    sql_df = sql_dfs()  # dict table name: table df
+    start5 = time.time()
+
+    for table_name in sql_df.keys():
+
+        if table_name == 'designers':
+            pk_column = 'designer_code'
+        else:
+            pk_column = 'id_obj'
+        new_data, changed_data, db_df = compare_dfs(excel_df[table_name], sql_df[table_name], table_name, pk_column)
+        print('Table name to upload:', table_name)
+
+        if not new_data.empty or not changed_data.empty:
+            if table_name == 'dd_list':
+                new_data['source_file'] = file_name
+                changed_data['source_file'] = file_name
+                new_data.to_sql(table_name, engine_dsn, if_exists='append', index=False)
+                changed_data.to_sql(table_name, engine_dsn, if_exists='append', index=False)
+            else:
+                all_data = pd.concat([db_df, changed_data, new_data], ignore_index=True)
+                # all_data = all_data.drop(columns='source')
+                all_data = all_data.drop_duplicates(subset=pk_column, keep='last', ignore_index=True)
+                all_data.to_sql(table_name, engine_dsn, if_exists='replace', index=False)
+
+    end5 = time.time()
+    print('upload to SQL time:', end5 - start5)
+
+
+
+start1 = time.time()
+# Load the uploaded file, if it exists
+if os.path.exists('uploaded_files.txt'):
+    with open('uploaded_files.txt', 'r', encoding="utf-8") as file:
+        uploaded_files = file.read().splitlines()
+else:
+    uploaded_files = []
+end1 = time.time()
+
+for filename in os.listdir(folder_path):
+    file_path = os.path.join(folder_path, filename)
+    if os.path.isfile(file_path) and filename not in uploaded_files:
+        print(f"Processing {filename}...")
+        try:
+            upload_to_sql(filename, file_path)
+            print(f'{filename} Done')
+        except Exception as e:
+            print(f"Error processing {filename}: {str(e)}")
+            continue
+
+        uploaded_files.append(filename)
+start2 = time.time()
+with open('uploaded_files.txt', 'w', encoding="utf-8") as file:
+    file.write('\n'.join(uploaded_files))
+end2 = time.time()
+
+print('Upload file read/write time:', end1-start1+end2-start2)
