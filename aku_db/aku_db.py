ab33acc53ea098d659efc18fff39198748c5b343
Arslan Vezirov <arslan.vezirov@gmail.com>
2023-10-05 14:29:05 +0300

    minor changes: Excel file updated -> DB2 code is completely changed used sqlalchemy instead of psycopg2 data successfully uploaded.

--- aku_db/aku_db.py
+++ aku_db/aku_db.py
@@ -1,57 +1,23 @@
 import pandas as pd
-import psycopg2
+from sqlalchemy import create_engine, MetaData
 
-# Connect to the PostgreSQL database
-conn = psycopg2.connect(
-    host="localhost",
-    port="5432",
-    database="AKUDB",
-    user="postgres",
-    password="postgres"
-)
+db_url = 'postgresql://postgres:postgres@localhost:5432/AKU_DB'
+excel_file = pd.ExcelFile(r'C:\Users\yuriy\Desktop\db_tables\AKUDB\DB2.xlsx')
 
-# Create a cursor object to execute SQL statements
-cursor = conn.cursor()
+# Create the SQLAlchemy engine and MetaData
+engine = create_engine(db_url)
+metadata = MetaData()
+# Reflect the database and load table names
+metadata.reflect(bind=engine)
 
-# Load the Excel file
-excel_file = pd.ExcelFile(r'C:\Users\yuriy\Desktop\db_tables\AKUDB\DB.xlsx')
 
-# Iterate over all sheets in the Excel file
-for sheet_name in excel_file.sheet_names:
-    # Read the sheet into a DataFrame
-    df = excel_file.parse(sheet_name)
+table_names_postgres = metadata.tables.keys()
+excel_sheet_names = pd.ExcelFile(excel_file).sheet_names
 
-    # Sanitize column names
-    df.columns = df.columns.str.replace(' ', '_', regex=True).str.replace('.', '', regex=True)
+# Find differences between table names and Excel sheet names
+postgres_only = set(table_names_postgres) - set(excel_sheet_names)
+excel_only = set(excel_sheet_names) - set(table_names_postgres)
 
-    # Define the table name based on the sheet name
-    table_name = sheet_name.lower().replace(' ', '_')
-
-    # Check if the table exists
-    cursor.execute(f"SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = '{table_name}')")
-    table_exists = cursor.fetchone()[0]
-
-    if table_exists:
-        print(f"Table '{table_name}' already exists. Skipping creation.")
-        continue
-
-    # Generate the CREATE TABLE statement based on the DataFrame columns
-    columns = ', '.join([f'"{column}" VARCHAR' for column in df.columns])
-    create_table_query = f'CREATE TABLE {table_name} ({columns})'
-
-    # Execute the CREATE TABLE statement
-    cursor.execute(create_table_query)
-
-    # Prepare the INSERT statement
-    insert_query = f'INSERT INTO {table_name} ({", ".join([f"{column}" for column in df.columns])}) ' \
-                   f'VALUES ({", ".join(["%s" for _ in df.columns])})'
-
-    # Insert the data from the DataFrame into the table
-    cursor.executemany(insert_query, df.values.tolist())
-
-    # Commit the changes for each sheet
-    conn.commit()
-
-# Close the cursor and the connection
-cursor.close()
-conn.close()
+for table_name in excel_sheet_names:
+    df = excel_file.parse(table_name)
+    df.to_sql(table_name, engine, if_exists='replace', index=False)
